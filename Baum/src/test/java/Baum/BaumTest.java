b1eaa95627b1a4721908298c5af91bccd254b2b3
Christian Laußmann <ch.laussmann@gmail.com>
2019-01-08 16:32:28 +0100

    Add Tests

---
+++ Baum/src/test/java/Baum/BaumTest.java
@@ -0,0 +1,66 @@
+package Baum;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BaumTest {
+
+    @Test
+    public void testAdd1(){
+        Baum baum = new Baum();
+        baum.add(22);
+        assertEquals(baum.wurzel.inhalt, 22);
+    }
+    @Test
+    public void testAdd2(){
+        Baum baum = new Baum();
+        baum.add(22);
+        baum.add(1);
+        assertEquals(baum.wurzel.inhalt, 22);
+        assertEquals(baum.wurzel.kleiner.inhalt, 1);
+    }
+    @Test
+    public void testAdd3(){
+        Baum baum = new Baum();
+        baum.add(22);
+        baum.add(1);
+        baum.add(100);
+        assertEquals(baum.wurzel.inhalt, 22);
+        assertEquals(baum.wurzel.kleiner.inhalt, 1);
+        assertEquals(baum.wurzel.groesser.inhalt, 100);
+    }
+    @Test
+    public void testFind1(){
+        Baum baum = new Baum();
+        baum.add(22);
+        baum.add(1);
+        baum.add(100);
+        assertEquals(baum.find(100), baum.wurzel.groesser);
+    }
+    @Test
+    public void testFind2(){
+        Baum baum = new Baum();
+        baum.add(22);
+        baum.add(1);
+        baum.add(100);
+        assertEquals(baum.find(22), baum.wurzel);
+    }
+    @Test
+    public void testFind3(){
+        Baum baum = new Baum();
+        baum.add(22);
+        baum.add(1);
+        baum.add(100);
+        assertEquals(baum.find(80) , null);
+    }
+    @Test
+    public void testContins(){
+        Baum baum = new Baum();
+        baum.add(22);
+        baum.add(1);
+        baum.add(100);
+        assert !baum.contains(80);
+        assert baum.contains(1);
+    }
+}
