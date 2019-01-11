04650d207785a5beed67fde8d1ec2c4701c99e01
Christian Laußmann <ch.laussmann@gmail.com>
2019-01-11 13:07:46 +0100

    loesung

--- Baum/src/main/java/Baum/Knoten.java
+++ Baum/src/main/java/Baum/Knoten.java
@@ -13,10 +13,20 @@ public class Knoten {
     }
 
     public Knoten find(int x){
+        if(inhalt == x) return this;
+        if(x < inhalt && kleiner != null) return  kleiner.find(x);
+        if(x > inhalt && groesser != null) return groesser.find(x);
         return null;
     }
 
     public void add(int x) {
-
+        if(x < inhalt){
+            if(kleiner == null) kleiner = new Knoten(x);
+            else kleiner.add(x);
+        }
+        if( x > inhalt){
+            if(groesser == null) groesser = new Knoten(x);
+            else groesser.add(x);
+        }
     }
 }
